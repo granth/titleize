c34b34bad6cca824ad1710f9d869cd433f463bae
Grant Hollingworth <grant@antiflux.org>
2008-05-21 19:40:40 -0400

    moved titlecase methods to their own describe block

--- spec/titlecase_spec.rb
+++ spec/titlecase_spec.rb
@@ -41,102 +41,104 @@ describe Titlecase do
     end
   end
 
-  it "should return a string" do
-    titlecase("this").should be_an_instance_of(String)
-  end
-
-  it "should capitalize the first letter of regular words" do
-    titlecase("cat beats monkey").should == "Cat Beats Monkey"
-  end
-
-  it "should not capitalize small words" do
-    SMALL_WORDS.each do |word|
-      titlecase("first #{word} last").should == "First #{word} Last"
+  describe "titlecase" do
+    it "should return a string" do
+      titlecase("this").should be_an_instance_of(String)
     end
-  end
 
-  it "should downcase a small word if it is capitalized" do
-    SMALL_WORDS.each do |word|
-      titlecase("first #{word.capitalize} last").should == "First #{word} Last"
+    it "should capitalize the first letter of regular words" do
+      titlecase("cat beats monkey").should == "Cat Beats Monkey"
     end
-  end
 
-  it "should capitalize a small word if it is the first word" do
-    SMALL_WORDS.each do |word|
-      titlecase("#{word} is small").should == "#{word.capitalize} Is Small"
-      titlecase("after: #{word} ok").should == "After: #{word.capitalize} Ok"
-      titlecase("after; #{word} ok").should == "After; #{word.capitalize} Ok"
-      titlecase("after. #{word} ok").should == "After. #{word.capitalize} Ok"
-      titlecase("after? #{word} ok").should == "After? #{word.capitalize} Ok"
-      titlecase("after! #{word} ok").should == "After! #{word.capitalize} Ok"
+    it "should not capitalize small words" do
+      SMALL_WORDS.each do |word|
+        titlecase("first #{word} last").should == "First #{word} Last"
+      end
     end
-  end
 
-  it "should capitalize a small word if it is the last word" do
-    SMALL_WORDS.each do |word|
-      titlecase("small #{word}").should == "Small #{word.capitalize}"
+    it "should downcase a small word if it is capitalized" do
+      SMALL_WORDS.each do |word|
+        titlecase("first #{word.capitalize} last").should == "First #{word} Last"
+      end
     end
-  end
 
-  it "should not screw up acronyms" do
-    titlecase("the SEC's decision").should == "The SEC's Decision"
-  end
+    it "should capitalize a small word if it is the first word" do
+      SMALL_WORDS.each do |word|
+        titlecase("#{word} is small").should == "#{word.capitalize} Is Small"
+        titlecase("after: #{word} ok").should == "After: #{word.capitalize} Ok"
+        titlecase("after; #{word} ok").should == "After; #{word.capitalize} Ok"
+        titlecase("after. #{word} ok").should == "After. #{word.capitalize} Ok"
+        titlecase("after? #{word} ok").should == "After? #{word.capitalize} Ok"
+        titlecase("after! #{word} ok").should == "After! #{word.capitalize} Ok"
+      end
+    end
 
-  it "should not capitalize words with dots" do 
-    titlecase("del.icio.us web site").should == "del.icio.us Web Site"
-  end
+    it "should capitalize a small word if it is the last word" do
+      SMALL_WORDS.each do |word|
+        titlecase("small #{word}").should == "Small #{word.capitalize}"
+      end
+    end
 
-  it "should not think a quotation mark makes a dot word" do
-    titlecase("'quoted.' yes.").should == "'Quoted.' Yes."
-    titlecase("ends with 'quotation.'").should == "Ends With 'Quotation.'"
-  end
+    it "should not screw up acronyms" do
+      titlecase("the SEC's decision").should == "The SEC's Decision"
+    end
 
-  it "should not capitalize words that have a lowercase first letter" do 
-    titlecase("iTunes").should == "iTunes"
-  end
+    it "should not capitalize words with dots" do 
+      titlecase("del.icio.us web site").should == "del.icio.us Web Site"
+    end
 
-  # http://daringfireball.net/projects/titlecase/examples-edge-cases
-  it "should handle edge cases" do
-    {
-      %{Q&A With Steve Jobs: 'That's What Happens In Technology'} =>
-        %{Q&A With Steve Jobs: 'That's What Happens in Technology'},
+    it "should not think a quotation mark makes a dot word" do
+      titlecase("'quoted.' yes.").should == "'Quoted.' Yes."
+      titlecase("ends with 'quotation.'").should == "Ends With 'Quotation.'"
+    end
 
-      %{What Is AT&T's Problem?} => %{What Is AT&T's Problem?},
+    it "should not capitalize words that have a lowercase first letter" do 
+      titlecase("iTunes").should == "iTunes"
+    end
 
-      %{Apple Deal With AT&T Falls Through} =>
-      %{Apple Deal With AT&T Falls Through},
+    # http://daringfireball.net/projects/titlecase/examples-edge-cases
+    it "should handle edge cases" do
+      {
+        %{Q&A With Steve Jobs: 'That's What Happens In Technology'} =>
+          %{Q&A With Steve Jobs: 'That's What Happens in Technology'},
 
-      %{this v that}   => %{This v That},
-      %{this vs that}  => %{This vs That},
-      %{this v. that}  => %{This v. That},
-      %{this vs. that} => %{This vs. That},
+        %{What Is AT&T's Problem?} => %{What Is AT&T's Problem?},
 
-      %{The SEC's Apple Probe: What You Need to Know} =>
-        %{The SEC's Apple Probe: What You Need to Know},
+        %{Apple Deal With AT&T Falls Through} =>
+        %{Apple Deal With AT&T Falls Through},
 
-      %{'by the Way, small word at the start but within quotes.'} =>
-        %{'By the Way, Small Word at the Start but Within Quotes.'},
+        %{this v that}   => %{This v That},
+        %{this vs that}  => %{This vs That},
+        %{this v. that}  => %{This v. That},
+        %{this vs. that} => %{This vs. That},
 
-      %{Small word at end is nothing to be afraid of} =>
-        %{Small Word at End Is Nothing to Be Afraid Of},
+        %{The SEC's Apple Probe: What You Need to Know} =>
+          %{The SEC's Apple Probe: What You Need to Know},
 
-      %{Starting Sub-Phrase With a Small Word: a Trick, Perhaps?} =>
-      %{Starting Sub-Phrase With a Small Word: A Trick, Perhaps?},
+        %{'by the Way, small word at the start but within quotes.'} =>
+          %{'By the Way, Small Word at the Start but Within Quotes.'},
 
-      %{Sub-Phrase With a Small Word in Quotes: 'a Trick, Perhaps?'} =>
-      %{Sub-Phrase With a Small Word in Quotes: 'A Trick, Perhaps?'},
+        %{Small word at end is nothing to be afraid of} =>
+          %{Small Word at End Is Nothing to Be Afraid Of},
 
-      %{Sub-Phrase With a Small Word in Quotes: "a Trick, Perhaps?"} =>
-      %{Sub-Phrase With a Small Word in Quotes: "A Trick, Perhaps?"},
+        %{Starting Sub-Phrase With a Small Word: a Trick, Perhaps?} =>
+        %{Starting Sub-Phrase With a Small Word: A Trick, Perhaps?},
 
-      %{"Nothing to Be Afraid of?"} => %{"Nothing to Be Afraid Of?"},
-      %{"Nothing to Be Afraid Of?"} => %{"Nothing to Be Afraid Of?"},
-      %{a thing} => %{A Thing},
+        %{Sub-Phrase With a Small Word in Quotes: 'a Trick, Perhaps?'} =>
+        %{Sub-Phrase With a Small Word in Quotes: 'A Trick, Perhaps?'},
 
-      %{'Gruber on OmniFocus and Vapo(u)rware'} =>
-      %{'Gruber on OmniFocus and Vapo(u)rware'},
-    }.each do |before, after|
-      titlecase(before).should == after
+        %{Sub-Phrase With a Small Word in Quotes: "a Trick, Perhaps?"} =>
+        %{Sub-Phrase With a Small Word in Quotes: "A Trick, Perhaps?"},
+
+        %{"Nothing to Be Afraid of?"} => %{"Nothing to Be Afraid Of?"},
+        %{"Nothing to Be Afraid Of?"} => %{"Nothing to Be Afraid Of?"},
+        %{a thing} => %{A Thing},
+
+        %{'Gruber on OmniFocus and Vapo(u)rware'} =>
+        %{'Gruber on OmniFocus and Vapo(u)rware'},
+      }.each do |before, after|
+        titlecase(before).should == after
+      end
     end
   end
 end
