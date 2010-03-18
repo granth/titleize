61b28bafea504e951c6a7b73b7364798cc0ca5f9
Grant Hollingworth <grant@antiflux.org>
2010-03-18 11:42:18 -0400

    bump version to 1.1.0

--- lib/titleize.rb
+++ lib/titleize.rb
@@ -5,7 +5,7 @@
 #
 # If loaded in a Rails environment, it modifies Inflector.titleize.
 module Titleize
-  VERSION = '1.0.1'
+  VERSION = '1.1.0'
   SMALL_WORDS = %w{a an and as at but by en for if in of on or the to v v. via vs vs.}
 
   extend self
