b1f74067e8712df1d4d5da0469c1b78185bd5a4d
Casey Stehlik <StehlikC@gmail.com>
2013-11-05 21:27:34 -0600

    Configure rspec to redirect deprecation warnings
    Add the log path to .gitignore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@ lib_path = File.expand_path("#{dir}/../lib")
 $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
 
 require 'titleize'
+
+RSpec.configure do |rspec|
+	rspec.deprecation_stream = 'log/deprecations.log'
+end
